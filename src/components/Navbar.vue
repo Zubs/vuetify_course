5abba321ac598ae3a9f7952362111cc7383777bd
Zubs <zubairidrisaweda@gmail.com>
2020-12-06 23:14:06 +0100

    Made drawer work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
 	<nav>
+
+		<!-- Navbar -->
 		<v-toolbar flat app>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer" class="pink--text"></v-app-bar-nav-icon>
 			<v-toolbar-title>
 				<span class="font-weight-light pink--text">AgbaDev</span>
 				<span class="text-uppercase blue--text text--lighten-3">Vuetify</span>
@@ -11,11 +14,19 @@
 				<v-icon right>mdi-exit-to-app</v-icon>
 			</v-btn>
 		</v-toolbar>
+
+		<v-navigation-drawer app class="indigo" v-model="drawer">
+			<p>Test</p>
+		</v-navigation-drawer>
 	</nav>
 </template>
 
 <script>
 	export default {
-		name: "Navbar"
+		data () {
+			return {
+				drawer: false
+			}
+		}
 	}
 </script>
