81eb685c5beb4fe6db39b694ab95fbd69de4514f
Zubs <zubairidrisaweda@gmail.com>
2020-12-06 19:07:26 +0100

    Started work on nav

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,21 @@
 <template>
-  <v-app>
-    <div id="app">
-      <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-      </div>
+  <v-app class="grey lighten-4">
+    <v-content>
+      <Navbar></Navbar>
       <router-view/>
-    </div>
+      <Footer></Footer>
+    </v-content>
   </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  import Navbar from './components/Navbar.vue'
+  import Footer from './components/Footer.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    components: {
+      Navbar,
+      Footer
     }
   }
-}
-</style>
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+	<nav>
+		<v-toolbar flat app>
+			<v-toolbar-title>
+				<span class="font-weight-light pink--text">AgbaDev</span>
+				<span class="text-uppercase blue--text text--lighten-3">Vuetify</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn color="pink" outlined>
+				<span>Logout</span>
+				<v-icon right>mdi-exit-to-app</v-icon>
+			</v-btn>
+		</v-toolbar>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: "Navbar"
+	}
+</script>
